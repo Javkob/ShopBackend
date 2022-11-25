45f51480eb78084ec587ae2b1d6dfc21a30c99bd
Javkob <javkob.witness@gmail.com>
2022-11-25 22:11:09 +0100

    Implemented Spring Data product page pagination

--- src/main/java/pl/javkob/shop/product/controller/ProductController.java
+++ src/main/java/pl/javkob/shop/product/controller/ProductController.java
@@ -2,13 +2,14 @@ package pl.javkob.shop.product.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.javkob.shop.product.model.Product;
 import pl.javkob.shop.product.service.ProductService;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +18,8 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    public Page<Product> getProducts(@PageableDefault(size = 25) Pageable pageable) {
+        return productService.getProducts(pageable);
     }
 
 }
--- src/main/java/pl/javkob/shop/product/service/ProductService.java
+++ src/main/java/pl/javkob/shop/product/service/ProductService.java
@@ -1,11 +1,12 @@
 package pl.javkob.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.javkob.shop.product.model.Product;
 import pl.javkob.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +15,7 @@ public class ProductService {
     private final ProductRepository productRepository;
 
 
-    public List<Product> getProducts(){
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable){
+        return productRepository.findAll(pageable);
     }
 }
